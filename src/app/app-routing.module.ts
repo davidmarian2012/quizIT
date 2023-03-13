0cb001a8e5462681ea27ba72385a1a8b618f5039
David <davidmarian2012@gmail.com>
2023-03-13 23:48:39 +0200

    last commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './modules/auth/components/login/login.component';
 import { RegisterComponent } from './modules/auth/components/register/register.component';
 import { DashboardComponent } from './modules/game/components/dashboard/dashboard.component';
+import { ProfileComponent } from './modules/shared/components/profile/profile.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'dashboard', component: DashboardComponent},
+  {path: 'profile', component: ProfileComponent},
   {path: '', redirectTo: 'login', pathMatch: 'full'}
 ];
 
